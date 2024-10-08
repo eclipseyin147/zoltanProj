d0b38a7314f2bfc3d7333884b0195174382e7cbc
eclipseyin147 <18501624902@163.com>
2024-10-08 16:24:04 +0800

    ptScotch support

---
+++ vcpkg/overlay-triplets/x64-linux.cmake
@@ -0,0 +1,20 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+
+if (DEFINED ENV{MARCH})
+    message("MARCH is set")
+    set(MARCH_SETTING $ENV{MARCH})
+else ()
+    message("MARCH is not set. Using default value")
+    set(MARCH_SETTING "x86-64")
+endif ()
+
+message("MARCH setting for VCPKG:  ${MARCH_SETTING}")
+
+
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -march=${MARCH_SETTING}")
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -march=${MARCH_SETTING}")
